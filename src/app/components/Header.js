1e3406eb2c8676ebf70ce2c5c1ce19972093e531
TaopikQM <taopik61318@gmail.com>
2024-06-13 21:17:06 +0700

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -69,7 +69,7 @@ const Header = () => {
             Beranda
           </a>
            <a
-            href="/admin"
+            href="#"
             className={`px-4 py-2 text-[22px] font-normal ${currentPath === '#' ? 'bg-blue-600 text-white rounded' : 'hover:bg-blue-600 hover:text-white hover:rounded'}`}
           >
             Promo
