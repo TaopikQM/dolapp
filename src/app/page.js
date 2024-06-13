7c14389f6a8edae54ff36d88f58b8a8b1e2b1505
TaopikQM <taopik61318@gmail.com>
2024-06-13 20:40:00 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,70 @@
-"use client";
+// src/app/page.js
+"use client"; // Tambahkan baris ini
 
-import React, { useState } from 'react';
+import React from "react";
+import Header from "./components/Header";
+import SearchAw from "./components/SearchAw";
+import ContainerAw from "./components/containerAw";
+import Box from "./components/Box";
+import TopCon from "./components/TopCon";
+import EventCalender from "./components/EventCalender";
+import Event from "./components/Event";
+import PaketWis from "./components/PaketWis";
+import TestimonialCarousel from "./components/TestimonialCarousel";
+import Footer from "./components/Footer";
 
-import ContainerAw from './components/containerAw';
 
-import AddProduct from './components/AddProduct';
+import RekomReal from "./components/RekomReal";
+import WisataBaru from "./components/WisataBaru";
 
-import ProductList from './components/ProductList';
-import Navbar from './components/Navbar';
+
+
 
 
 export default function Home() {
-  const [showForm, setShowForm] = useState(false);
+  const events = [
+    { id: 1, title: 'Meeting', date: '2024-05-10' },
+    { id: 2, title: 'Conference', date: '2024-05-15' },
+    { id: 3, title: 'Vacation', date: '2024-05-20' },
+  ];
 
-  const toggleForm = ({ Component, pageProps }) => {
-    setShowForm(!showForm);
-  };
-  // Your component logic here
+  
+
   return (
     
-     <div>
-     <Navbar/>
-        <ContainerAw>
+    
+      
+    
+    <div>
+      
+      <Header />
+      <SearchAw />
+      <ContainerAw>
+        <Box/>
+        
+       {/**<RekomWis/> */} 
+        <RekomReal/>
+        
+        <WisataBaru/>
+        {/**<TempatBa/>*/} 
+        <TopCon/>
+        <EventCalender events={events} />
+        <Event/>
+        <PaketWis />
+        <TestimonialCarousel />
+        
+      </ContainerAw>
+      <Footer/>
+    
+     {/*<div className="container mx-auto p-4">
+      
+        <h1 className="text-2xl font-bold mb-4">Wisata</h1>
+        
+        <AddPlaceForm />
+        <PlaceTable />
 
-            <div className="flex items-center justify-center">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={toggleForm}>Add Product</button>
-            </div>
-            {showForm && <AddProduct />}
-            <br/>
-          
-        <ProductList/>
-        </ContainerAw>
-   </div>
+  </div>*/}
+    </div>
    
   );
 }
