bcf0273c63c0155021c01c05a2b35702a135c633
TaopikQM <taopik61318@gmail.com>
2024-06-28 00:02:28 +0700

    Update page.js

--- src/app/api/page.js
+++ src/app/api/page.js
@@ -1,38 +1,35 @@
 import { ref, set } from 'firebase/database';
 import { rtdb } from '../../config/firebase';
 
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const notification = req.body;
 
-    // Log request body (bisa diganti dengan penyimpanan ke database)
+    // Log request body for debugging
     console.log('Notification received:', notification);
 
+    // Reference to the transaction in the Realtime Database
     const transactionRef = ref(rtdb, 'transactions/' + notification.order_id);
 
-   try {
-      await  set(transactionRef,{
+    try {
+      await set(transactionRef, {
         transaction_time: notification.transaction_time,
         transaction_status: notification.transaction_status,
         transaction_id: notification.transaction_id,
         status_message: notification.status_message,
         status_code: notification.status_code,
         signature_key: notification.signature_key,
+        settlement_time: notification.settlement_time, // Add this line
         payment_type: notification.payment_type,
         order_id: notification.order_id,
         merchant_id: notification.merchant_id,
-        masked_card: notification.masked_card,
         gross_amount: notification.gross_amount,
         fraud_status: notification.fraud_status,
-        eci: notification.eci,
+        expiry_time: notification.expiry_time, // Add this line
         currency: notification.currency,
-        channel_response_message: notification.channel_response_message,
-        channel_response_code: notification.channel_response_code,
-        card_type: notification.card_type,
-        bank: notification.bank,
-        approval_code: notification.approval_code
-        // field lainnya jika diperlukan
+        va_numbers: notification.va_numbers, // Add this line
+        payment_amounts: notification.payment_amounts // Add this line
+        // Add other fields as necessary
       });
       res.status(200).send('Notification saved successfully');
     } catch (error) {
